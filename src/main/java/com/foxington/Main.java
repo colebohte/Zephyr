4297e50d966ddf9351b99ab96bfa60bfb343fe65
colebohte <colebohte@rivergames.ca>
2026-09-19 13:18:12 -0400

    Default window wize updated to 480p

--- src/main/java/com/foxington/Main.java
+++ src/main/java/com/foxington/Main.java
@@ -1,8 +1,3 @@
-// (c) Copyright Cole Bohte
-// Zephyr is licensed under
-
-package com.foxington;
-
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
@@ -12,26 +7,13 @@ import java.nio.*;
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main {
-
     private long window;
 
     public void run() {
-        init();
-        loop();
-
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
-    }
-
-    private void init() {
-        GLFWErrorCallback.createPrint(System.err).set();
-
         if (!glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
@@ -40,29 +22,43 @@ public class Main {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-        window = glfwCreateWindow(854, 480, "Zephyr Engine", NULL, NULL);
+        window = glfwCreateWindow(854, 480, "Zephyr", NULL, NULL);
         if (window == NULL) {
-            throw new RuntimeException("Failed to create GLFW window");
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+            glfwGetWindowSize(window, pWidth, pHeight);
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
         }
 
         glfwMakeContextCurrent(window);
         glfwSwapInterval(1);
-
         glfwShowWindow(window);
-    }
 
-    private void loop() {
         GL.createCapabilities();
 
-        glClearColor(0.2f, 0.4f, 0.6f, 0.0f);
+        glClearColor(0.1f, 0.1f, 0.15f, 0.0f);
 
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
             glfwSwapBuffers(window);
-
             glfwPollEvents();
         }
+
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     public static void main(String[] args) {
